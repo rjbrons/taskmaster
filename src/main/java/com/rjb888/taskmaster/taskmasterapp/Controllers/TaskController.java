55974e4277fc61c6e7636c08795c1e122984c1da
Robert Bronson <robert.j.bronson+cf@gmail.com>
2019-07-02 13:14:48 -0700

    cant figure out the post requests

--- src/main/java/com/rjb888/taskmaster/taskmasterapp/Controllers/TaskController.java
+++ src/main/java/com/rjb888/taskmaster/taskmasterapp/Controllers/TaskController.java
@@ -4,7 +4,10 @@ import com.rjb888.taskmaster.taskmasterapp.Models.Task;
 import com.rjb888.taskmaster.taskmasterapp.Repos.TaskRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -18,4 +21,16 @@ public class TaskController {
         return taskRepo.findAll();
     }
 
+    @PostMapping("/tasks")
+    public RedirectView newTask(){
+
+        return new RedirectView("/");
+    }
+
+    @PutMapping("/tasks/{id}/state")
+    public RedirectView updateTask(){
+        // if (this.status == "available") ... etc.  update state accordingly
+
+        return new RedirectView("/");
+    }
 }
