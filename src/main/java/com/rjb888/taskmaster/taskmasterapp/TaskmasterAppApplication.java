3b61874ef658966d1a9a01216226d3eed70e6a41
Robert Bronson <robert.j.bronson+cf@gmail.com>
2019-07-02 09:51:37 -0700

    initial commit

---
+++ src/main/java/com/rjb888/taskmaster/taskmasterapp/TaskmasterAppApplication.java
@@ -0,0 +1,13 @@
+package com.rjb888.taskmaster.taskmasterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmasterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmasterAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rjb888/taskmaster/taskmasterapp/TaskmasterAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rjb888.taskmaster.taskmasterapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskmasterAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
